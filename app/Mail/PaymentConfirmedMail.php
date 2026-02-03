e9bc3eaf0e83c706dcd03318f81de51ea13e29b7
kdkhost <marcelobradrj@gmail.com>
2026-02-03 14:18:38 -0300

    feat: Implement Email Queueing (ShouldQueue) and add jobs table

--- app/Mail/PaymentConfirmedMail.php
+++ app/Mail/PaymentConfirmedMail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PaymentConfirmedMail extends Mailable
+class PaymentConfirmedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class WelcomeMail extends Mailable
+class WelcomeMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
