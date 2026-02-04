fbebfe54ec201585a00c61907669532c4bc4154b
kdkhost <marcelobradrj@gmail.com>
2026-02-04 01:00:13 -0300

    debug: Restore Menu Block 1 (Courses to Plans)

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -28,7 +28,63 @@
                         <p>Dashboard</p>
                     </a>
                 </li>
-                <!-- DEBUG: REST OF MENU TEMPORARILY HIDDEN TO CHECK IF CRASH IS ABOVE -->
+
+                <li class="nav-item has-treeview {{ $open('admin.courses.*') }}">
+                    <a href="#" class="nav-link {{ $is('admin.courses.*') }}">
+                        <i class="nav-icon fas fa-graduation-cap"></i>
+                        <p>Cursos<i class="right fas fa-angle-left"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview pl-4">
+                        <li class="nav-item"><a href="{{ route('admin.courses.index') }}" class="nav-link {{ $is('admin.courses.index') }}"><i class="fas fa-list nav-icon"></i><p>Listar</p></a></li>
+                        <li class="nav-item"><a href="{{ route('admin.courses.create') }}" class="nav-link {{ $is('admin.courses.create') }}"><i class="fas fa-plus nav-icon"></i><p>Novo</p></a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item has-treeview {{ $open('admin.users.*') }}">
+                    <a href="#" class="nav-link {{ $is('admin.users.*') }}">
+                        <i class="nav-icon fas fa-users-cog"></i>
+                        <p>Usuários<i class="right fas fa-angle-left"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview pl-4">
+                        <li class="nav-item"><a href="{{ route('admin.users.index') }}" class="nav-link {{ $is('admin.users.index') }}"><i class="fas fa-list nav-icon"></i><p>Listar</p></a></li>
+                        <li class="nav-item"><a href="{{ route('admin.users.create') }}" class="nav-link {{ $is('admin.users.create') }}"><i class="fas fa-plus nav-icon"></i><p>Novo</p></a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item has-treeview {{ $open('admin.events.*') }}">
+                    <a href="#" class="nav-link {{ $is('admin.events.*') }}">
+                        <i class="nav-icon fas fa-calendar"></i>
+                        <p>Eventos<i class="right fas fa-angle-left"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview pl-4">
+                        <li class="nav-item"><a href="{{ route('admin.events.index') }}" class="nav-link {{ $is('admin.events.index') }}"><i class="fas fa-calendar-alt nav-icon"></i><p>Calendário</p></a></li>
+                        <li class="nav-item"><a href="{{ route('admin.events.create') }}" class="nav-link {{ $is('admin.events.create') }}"><i class="fas fa-plus nav-icon"></i><p>Novo</p></a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item has-treeview {{ $open('admin.mentorships.*') }}">
+                    <a href="#" class="nav-link {{ $is('admin.mentorships.*') }}">
+                        <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                        <p>Mentorias<i class="right fas fa-angle-left"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview pl-4">
+                        <li class="nav-item"><a href="{{ route('admin.mentorships.index') }}" class="nav-link {{ $is('admin.mentorships.index') }}"><i class="fas fa-list nav-icon"></i><p>Listar</p></a></li>
+                        <li class="nav-item"><a href="{{ route('admin.mentorships.create') }}" class="nav-link {{ $is('admin.mentorships.create') }}"><i class="fas fa-plus nav-icon"></i><p>Novo</p></a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item has-treeview {{ $open('admin.plans.*') }}">
+                    <a href="#" class="nav-link {{ $is('admin.plans.*') }}">
+                        <i class="nav-icon fas fa-tags"></i>
+                        <p>Planos<i class="right fas fa-angle-left"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview pl-4">
+                        <li class="nav-item"><a href="{{ route('admin.plans.index') }}" class="nav-link {{ $is('admin.plans.index') }}"><i class="fas fa-list nav-icon"></i><p>Listar</p></a></li>
+                        <li class="nav-item"><a href="{{ route('admin.plans.create') }}" class="nav-link {{ $is('admin.plans.create') }}"><i class="fas fa-plus nav-icon"></i><p>Novo</p></a></li>
+                    </ul>
+                </li> 
+
+                <!-- DEBUG: Remaining items hidden (Certificates, Emails, Points, Permissions, Sales, Community) -->
             </ul>
         </nav>
     </div>
