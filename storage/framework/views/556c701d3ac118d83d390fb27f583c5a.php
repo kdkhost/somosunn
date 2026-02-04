93d8d54fcfa1315107e93bdc5dcdee370e0b4af6
kdkhost <marcelobradrj@gmail.com>
2026-02-04 13:40:22 -0300

    Refinando o visual dos e-mails: Preview e Teste agora usam o layout oficial com Logo e Cores do Admin 🎨✨

--- storage/framework/views/556c701d3ac118d83d390fb27f583c5a.php
+++
@@ -1,4 +0,0 @@
-<footer class="main-footer py-2">
-    <div class="float-right d-none d-sm-inline"><b>Versão</b> 0.1</div>
-    <strong>&copy; <?php echo e(date('Y')); ?> UNN</strong>
-</footer><?php /**PATH /home/somosunn/public_html/resources/views/admin/partials/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/c6ed492d195f93d5bc43ac3f78e6e7b7.php
+++
@@ -1,3 +0,0 @@
-
-
-<?php echo $__env->make('partials.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/somosunn/public_html/resources/views/partials/header.blade.php ENDPATH**/ ?>
