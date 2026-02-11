343e1b543f95ca566525662972bc92a1926130ce
kdkhost <kdkhost@users.noreply.github.com>
2026-02-10 21:12:19 -0300

    fix: Remove Queueable de CertificateIssued para envio imediato - Emails agora sao enviados sincronamente - Nao dependem mais do queue worker - Resolve problema de emails nao chegando

--- app/Mail/CertificateIssued.php
+++ app/Mail/CertificateIssued.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Storage;
 
 class CertificateIssued extends Mailable
 {
-    use Queueable, SerializesModels;
+    use SerializesModels;
 
     public $certificate;
     public $user;
