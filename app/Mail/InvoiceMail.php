7c3817c31acf2366a46e643a6149592e7cd44cf6
kdkhost <kdkhost@users.noreply.github.com>
2026-02-25 22:04:29 -0300

    fix: evitar 500 no envio de fatura por payload UTF-8 malformado

--- app/Mail/InvoiceMail.php
+++ app/Mail/InvoiceMail.php
@@ -6,11 +6,10 @@ use App\Models\Invoice;
 use App\Services\InvoiceService;
 use App\Services\Mail\SystemMailLayoutData;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class InvoiceMail extends Mailable implements ShouldQueue
+class InvoiceMail extends Mailable
 {
     use Queueable, SerializesModels;
 
