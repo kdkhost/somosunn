90e147a28aa5cd6edaaa9f2c9d86e0954a7fbf81
kdkhost <kdkhost@users.noreply.github.com>
2026-03-05 22:04:53 -0300

    Alinha a edição de mentorias ao layout novo do painel admin

--- resources/views/panel/admin/mentorships/form.blade.php
+++ resources/views/panel/admin/mentorships/form.blade.php
@@ -2,7 +2,11 @@
 
 @section('title', $mentorship->exists ? 'Editar Mentoria: ' . $mentorship->title : 'Nova Mentoria')
 
-@section('content')
+@section('panel_breadcrumb')
+    <a href="{{ route('panel.admin.mentorships.index') }}" class="hover:underline">Mentorias</a>
+@endsection
+
+@section('panel_content')
     <div x-data="{ tab: 'general' }" class="space-y-6">
         {{-- Header --}}
         <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
